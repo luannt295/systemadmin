fd3575579032a391b6ed0f056d2244d4d336eb13
luannt <luan.nt295@gmail.com>
2025-05-11 18:50:37 +0700

    modified

--- scanport.py
+++ scanport.py
@@ -1,29 +1,29 @@
 from socket import *
-def conScan(tgtHost, tgtPort):
+def ScanHost(tarHost, tarPort):
     try:
-        connskt = socket(AF_INET, SOCK_STREAM)
-        connskt.connect((tgtHost, tgtPort))
-        print('[+]%d/tcp open'% tgtPort)
-        connskt.close()
+        packet = socket(AF_INET, SOCK_STREAM) #AF_INET:ipv4, SOC_STREAM: TCP/IP
+        packet.connect((tarHost, tarPort))
+        print('[+]%d/tcp open'% tarPort)
+        packet.close()
     except:
-        print('[-]%d/tcp closed'% tgtPort)
+        print('[-]%d/tcp closed'% tarPort)
 
-def portScan(tgtHost, tgtPorts):
+def ScanPort(tarHost, tgtPorts):
     try:
-        tgtIP = gethostbyname(tgtHost)
+        tarIP = gethostbyname(tarHost)
     except:
-        print('[-] Cannot resolve %s '% tgtHost)
+        print('[-] Cannot resolve %s '% tarHost)
         return
 
     try:
-        tgtName = gethostbyaddr(tgtIP)
-        print('\n[+] Scan result of: %s ' % tgtName[0])
+        tarName = gethostbyaddr(tarIP)
+        print('\n[+] Scan result of: %s ' % tarName[0])
     except:
-        print('\n[+] Scan result of: %s ' % tgtIP)
+        print('\n[+] Scan result of: %s ' % tarIP)
     setdefaulttimeout(1)
-    for tgtPort in tgtPorts:
-        print('Scanning Port: %d'% tgtPort)
-        conScan(tgtHost, int(tgtPort))
+    for tarPort in tgtPorts:
+        print('Scanning Port: %d'% tarPort)
+        ScanHost(tarHost, int(tarPort))
 
 if __name__=='__main__':
-    portScan('hbcg.vn', [80, 22])
+    ScanPort('hbcg.vn', [80, 10051])
